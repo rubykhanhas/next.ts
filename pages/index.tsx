2993e370a87345726a0b42fda7f7b46c8c015cdb
linux <rubykhanhas@gmail.com>
2021-01-20 16:11:56 +0700

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import { Container } from 'reactstrap';
+
+export default function Home() {
+    return <Container xs="auto" md="6">
+        <h2>Helo world!</h2>
+    </Container>
+}
